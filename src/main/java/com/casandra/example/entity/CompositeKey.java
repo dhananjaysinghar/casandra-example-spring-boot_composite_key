9fa4e58b6f6ae64162263d7aecdac1f3bb25afb8
Dhananjaya Samanta Singhar <52064780+dhananjaysinghar@users.noreply.github.com>
2022-09-08 18:23:24 +0530

    Update CompositeKey.java

--- src/main/java/com/casandra/example/entity/CompositeKey.java
+++ src/main/java/com/casandra/example/entity/CompositeKey.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.cassandra.core.cql.Ordering;
 import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
+import org.springframework.data.cassandra.core.mapping.CassandraType;
 import org.springframework.data.cassandra.core.mapping.PrimaryKeyClass;
 import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
 
@@ -14,10 +15,12 @@ import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
 @PrimaryKeyClass
 public class CompositeKey {
 
-    @PrimaryKeyColumn(name = "USERID", ordinal = 0, type = PrimaryKeyType.PARTITIONED, ordering = Ordering.ASCENDING)
+    @PrimaryKeyColumn(name = "USERID", ordinal = 0, type = PrimaryKeyType.PARTITIONED)
+    @CassandraType(type = CassandraType.Name.VARCHAR)
     private String userId;
 
-    @PrimaryKeyColumn(name = "PROFILEID", ordinal = 1, type = PrimaryKeyType.PARTITIONED)
+    @PrimaryKeyColumn(name = "PROFILEID", ordinal = 1, type = PrimaryKeyType.CLUSTERED, ordering = Ordering.ASCENDING)
+    @CassandraType(type = CassandraType.Name.VARCHAR)
     private String profileId;
 
 }
